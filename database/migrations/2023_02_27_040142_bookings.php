c9e18af03af3270caeadf688a03601f4905ef43a
danghuyhoang <danghuyhoang26052002@gmail.com>
2023-02-27 13:40:31 +0700

    DB migration

---
+++ database/migrations/2023_02_27_040142_bookings.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->date('date_from');
+            $table->date('checkin_date');
+            $table->date('checkout_date');
+            $table->string('people');
+            $table->string('cate_room_id');
+            $table->string('status_booking');
+            $table->string('status_pay');
+            $table->string('id_nhanvien');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
---
+++ database/migrations/2023_02_27_040209_bill_details.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bill_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('service_id');
+            $table->string('room_id');
+            $table->string('bill_id');
+            $table->string('quantity');
+            $table->date('date');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bill_details');
+    }
+};
