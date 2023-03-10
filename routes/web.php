ab9d23fc526c0f949cd96dea26d4b18a219e83db
danghuyhoang <danghuyhoang26052002@gmail.com>
2023-03-10 21:05:20 +0700

    Roomss

--- routes/web.php
+++ routes/web.php
@@ -75,9 +75,10 @@ Route::prefix('admin')->group(function () {
     });
 
     Route::prefix('/rooms')->group(function () {
+
         Route::get('/', 'App\Http\Controllers\Admin\RoomController@rooms')->name('route_BackEnd_Rooms_List');
-        Route::match(['get', 'post'], '//add', 'App\Http\Controllers\Admin\RoomController@rooms_add')->name('route_BackEnd_Rooms_Add');
-        Route::get('/detail', 'App\Http\Controllers\Admin\RoomController@rooms_detail')->name('route_BackEnd_Rooms_Detail');
+        Route::match(['post','get'], '/add', 'App\Http\Controllers\Admin\RoomController@rooms_add')->name('route_BackEnd_Rooms_Add');
+        Route::get('/detail/{id}', 'App\Http\Controllers\Admin\RoomController@rooms_detail')->name('route_BackEnd_Rooms_Detail');
         Route::post('/update/{id}', 'App\Http\Controllers\Admin\RoomController@rooms_update')->name('route_BackEnd_Rooms_Update');
         Route::get('/remove/{id}', 'App\Http\Controllers\Admin\RoomController@rooms_remove')->name('route_BackEnd_Rooms_Remove');
     });
