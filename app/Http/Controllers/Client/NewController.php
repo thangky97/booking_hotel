c1959cf90915c5648ffde8333066d18106479af0
thangtv <tranthangvui360@gmail.com>
2023-03-30 12:40:28 +0700

    controller new client

--- app/Http/Controllers/Client/NewController.php
+++ app/Http/Controllers/Client/NewController.php
@@ -10,45 +10,11 @@ use Illuminate\Http\Request;
 class NewController extends Controller
 {
 
-    private $v;
-
-    public function __construct()
-    {
-        $this->v = [];
-    }
-
-    // public function index()
-    // {
-    //     $this->v['title'] = '12Zodiac - Bài viết';
-
-    //     $this->v['news'] = News::select('id','name', 'images', 'description', 'title', 'date', 'cate_id', 'status')
-    //         ->where('status', '=', 1)
-    //         ->get();
-
-    //     return view('templates.pages.new', $this->v);
-    // }
-
-    // public function index(Request $request) {
-    //     $this->v['title'] = '12Zodiac - Bài viết';
-    //     $new = new News();
-    //     $this->v['extParams'] = $request->all();
-    //     $this->v['news'] = $new->loadList($this->v['extParams']);
-    //     dd($this->v['news']);
-    //     return view('templates.pages.new', $this->v);
-    // }
-
     public function index(Request $request) {
-        $title = '12Zodiac - Bài viết';
+        $title = '12Zodiac - Tin tức';
         $news = News::with('cate_new')->get();
         return view('templates.pages.new', compact('news', 'title'));
     }
-
-    // public function detail($id, Request $request) {
-    //     $detail = new News();
-    //     $objItem = $detail->loadOne($id);
-    //     $this->v['objItem'] = $objItem;
-    //     return view('client.layouts.detail', $this->v);
-    // }
 
     public function detail($id, Request $request)
     {    
