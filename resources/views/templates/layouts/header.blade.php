7b0d8abeea1d5f5d0e94af72e3ff1dfd14a60825
danghuyhoang <danghuyhoang26052002@gmail.com>
2023-04-11 22:32:50 +0700

    update UI 11/4

--- resources/views/templates/layouts/header.blade.php
+++ resources/views/templates/layouts/header.blade.php
@@ -714,7 +714,7 @@
                             <div class="elementor-element elementor-element-85b82e9 elementor-align-right elementor-tablet-align-center elementor-widget elementor-widget-button" data-id="85b82e9" data-element_type="widget" data-widget_type="button.default">
                                 <div class="elementor-widget-container">
                                     <div class="elementor-button-wrapper">
-                                        <a href="#flatbooking" class="elementor-button-link elementor-button elementor-size-sm" role="button">
+                                        <a href="/#flatbooking" class="elementor-button-link elementor-button elementor-size-sm" role="button">
                                             <span class="elementor-button-content-wrapper">
                                                 <span class="elementor-button-text">Đặt Phòng cho Cặp Đôi</span>
                                             </span>
